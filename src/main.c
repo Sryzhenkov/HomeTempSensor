81a5a998671ed13d8e32fd334ac8185c4568d724
Serhii Ryzhenkov <Sergiy905@gmail.com>
2023-02-10 17:03:36 +0200

    #0.2 garbage

--- src/main.c
+++ src/main.c
@@ -13,7 +13,7 @@ int LedInit(const struct gpio_dt_spec*, gpio_flags_t);
 Global_Info_t globalInfo_s = {
 								.bmp280 = I2C_DT_SPEC_GET(BMP280_NODE),
 }; 
-
+/*
 void main(void)
 {	
 	//LedInit(&ledRed,GPIO_OUTPUT_ACTIVE);
@@ -30,3 +30,29 @@ void main(void)
 			BMP280_ReadTemp();	
 		}
 }
+*/
+#define STACKSIZE 1024
+
+#define THREAD0_PRIORITY 7
+#define THREAD1_PRIORITY 7
+
+void thread0(void)
+{
+	while (1) {
+		  LOG_INF("Hello, I am thread0\n");
+          //k_yield();
+		  k_msleep(1000);
+	}
+}
+
+void thread1(void)
+{
+	while (1) {
+          LOG_INF("Hello, I am thread1\n");		  
+		  //k_yield();		  
+		  k_msleep(1000);
+	}
+}
+
+K_THREAD_DEFINE(thread0_id, STACKSIZE, thread0, NULL, NULL, NULL, THREAD0_PRIORITY, 0, 0);
+K_THREAD_DEFINE(thread1_id, STACKSIZE, thread1, NULL, NULL, NULL, THREAD1_PRIORITY, 0, 0);
